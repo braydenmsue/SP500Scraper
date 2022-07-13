0e6ffb88c607486fc806c837feb87661cd88ca8d
braydenmsue <100061408+braydenmsue@users.noreply.github.com>
2022-07-12 20:09:02 -0700

    Add files via upload

--- stock_data_interpreter.py
+++ stock_data_interpreter.py
@@ -1,5 +1,7 @@
 import csv
+import pandas as pd
 
+df = pd.read_csv('stock_data.csv')
 file = open("stock_data.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
@@ -14,10 +16,12 @@ currentIndex = 0
 companyName = []
 percentChanges = []
 
+
 for row in csvreader:
     companyName.append(row[1])
     # Add percent change to list without the percent sign, so it can be converted to float and compared
-    percentChanges.append(float(row[2].strip("%")))
+    percentChanges.append(float(row[-1].strip("%")))
+
 
     # Compare current percent change value to highest so far, replace if current is higher
     if percentChanges[currentIndex] > highValue:
@@ -30,7 +34,6 @@ for row in csvreader:
         lowIndex = currentIndex
 
     currentIndex += 1
-
 
 print("Strongest performer of the day is " + companyName[highIndex] + " with a stock value increase of " + str(highValue) + "%.")
 print("Weakest performer of the day is " + companyName[lowIndex] + " with a stock value decrease of " + str(lowValue) + "%.")
